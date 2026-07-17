de4a026cf396e19c8b113db8ace8da81a6f3612a
Jear-jDev <132642448+Jiarrrr1@users.noreply.github.com>
2026-07-17 14:35:23 +0800

    feat: update .env configuration and enhance db connection logging

--- config/db_config.js
+++ config/db_config.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+
+console.log("MONGO_DB_ONLINE:", process.env.MONGO_DB_ONLINE ? "✅ Defined" : "❌ Undefined");
+console.log("MONGO_DB_LOCAL:", process.env.MONGO_DB_LOCAL ? "✅ Defined" : "❌ Undefined");
+
 mongoose
-  // .connect(process.env.MONGO_DB_ONLINE, {})
-  .connect(process.env.MONGO_DB_LOCAL, {})
-  .then(() => console.log("Connected to Database"))
-  .catch((err) => console.error("Error connecting to Database", err));
+  .connect(process.env.MONGO_DB_ONLINE, {})
+  // .connect(process.env.MONGO_DB_LOCAL, {})
+  .then(() => console.log("✅ Connected to Database"))
+  .catch((err) => console.error("❌ Error connecting to Database", err));
